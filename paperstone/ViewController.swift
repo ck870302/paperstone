9c453cc5990bf58c7dee7f651ca4d32ef70d66d6
“ChengYuHan” <ck870302@gmail.comgit config --global user.name ChengYuHangit -- globalgit config --global user.email ck870302@gmail.com>
2018-05-11 20:20:31 +0800

    Initial Commit

---
+++ paperstone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  paperstone
+//
+//  Created by 鄭羽涵 on 2018/5/11.
+//  Copyright © 2018年 Haley YH Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
